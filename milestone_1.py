6f4d01bf7671e47b756652515f18e06f0ee2a8d0
chair03 <64752456+chair03@users.noreply.github.com>
2022-09-26 20:51:00 +0100

    Add user input

--- milestone_1.py
+++ milestone_1.py
@@ -1,4 +1,12 @@
 import random
+import string
+
+guess = input("Enter a single character ")
+
+if len(guess) == 1 and (guess in string.ascii_lowercase or guess in string.ascii_uppercase):
+    print('Good guess!')
+else:
+    print("Oops! That is not a valid input.")
 
 word_list = ['apple','orange','pear','rasberry','lime']
 word = random.choice(word_list)
