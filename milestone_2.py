0fc960ebb9ce5f056a9f5a61009909334a221d82
chair03 <64752456+chair03@users.noreply.github.com>
2022-09-27 13:26:01 +0100

    making functions

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,23 @@
 import string 
-while(True):
-    guess = input("Enter a single character ")
-    if len(guess) == 1 and (guess in string.ascii_lowercase or guess in string.ascii_uppercase):
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+import random
 
+word_list = ['apple','orange','pear','rasberry','lime']
+word = random.choice(word_list)
+
+
+def check_guess(guess:str)->bool:
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+    
+def ask_for_input():
+    while(True):
+        guess = input("Enter a single character ")
+        if len(guess) == 1 and (guess in string.ascii_lowercase or guess in string.ascii_uppercase):
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+ask_for_input()
